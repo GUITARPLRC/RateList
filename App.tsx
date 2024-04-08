7a3e68fac08a6019b6a00c647b667c7054ab8d0a
Chuck Reynolds <guitarplrc@yahoo.com>
2024-04-07 22:01:19 -0500

    feat: add splash timeout

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet, View } from "react-native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { NavigationContainer } from "@react-navigation/native"
 import AuthProvider from "./src/context/auth"
@@ -7,27 +7,67 @@ import Entry from "./src/views/Entry"
 import { colors } from "./src/styles"
 import { ModalPortal } from "react-native-modals"
 import { RootSiblingParent } from "react-native-root-siblings"
+import { useState, useEffect, useCallback } from "react"
+import * as SplashScreen from "expo-splash-screen"
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  return (
-    <RootSiblingParent>
-      <SafeAreaProvider style={styles.safeArea}>
-        <AuthProvider>
-          <MyListsProvider>
-            <NavigationContainer>
-              <Entry />
-              <ModalPortal />
-            </NavigationContainer>
-          </MyListsProvider>
-        </AuthProvider>
-      </SafeAreaProvider>
-    </RootSiblingParent>
-  )
+	const [appIsReady, setAppIsReady] = useState(false)
+
+	useEffect(() => {
+		async function prepare() {
+			try {
+				// Artificially delay for two seconds to simulate a slow loading
+				// experience. Please remove this if you copy and paste the code!
+				await new Promise((resolve) => setTimeout(resolve, 1000))
+			} catch (e) {
+				console.warn(e)
+			} finally {
+				// Tell the application to render
+				setAppIsReady(true)
+			}
+		}
+
+		prepare()
+	}, [])
+
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			// This tells the splash screen to hide immediately! If we call this after
+			// `setAppIsReady`, then we may see a blank screen while the app is
+			// loading its initial state and rendering its first pixels. So instead,
+			// we hide the splash screen once we know the root view has already
+			// performed layout.
+			await SplashScreen.hideAsync()
+		}
+	}, [appIsReady])
+
+	if (!appIsReady) {
+		return null
+	}
+	return (
+		<View onLayout={onLayoutRootView} style={styles.safeArea}>
+			<RootSiblingParent>
+				<SafeAreaProvider style={styles.safeArea}>
+					<AuthProvider>
+						<MyListsProvider>
+							<NavigationContainer>
+								<Entry />
+								<ModalPortal />
+							</NavigationContainer>
+						</MyListsProvider>
+					</AuthProvider>
+				</SafeAreaProvider>
+			</RootSiblingParent>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: colors.darkBackground,
-  },
+	safeArea: {
+		flex: 1,
+		backgroundColor: colors.darkBackground,
+	},
 })
